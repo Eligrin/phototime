d21e2796f2a2dbc57d112ec2b86936b71a08eebd
Eligrin <63203119+Eligrin@users.noreply.github.com>
2020-04-27 01:33:27 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+	$('.burger').click(function (event) {
+		$('.burger, .menu, .header').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
+
+
+$(document).ready(function () {
+	$('.slider').slick({
+		arrows: true,
+		dots: false,
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		appendArrows: $('.arrows'),
+		// centerMode: true,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					// centerMode: true,
+					appendArrows: $('.arrows_mobile'),
+				}
+			}
+		],
+	});
+});
+
